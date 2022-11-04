fa1b9fb571307bfc773dbb45b8cf82b4929fa888
ilhamfatkur <ideveloper210698@gmail.com>
2022-11-04 14:45:51 +0700

    Send Message Complate

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:riset_chatapp_flutter/pages/home_page.dart';
 import 'package:riset_chatapp_flutter/service/database_service.dart';
+import 'package:riset_chatapp_flutter/widgets/widgets.dart';
 
 class GroupInfo extends StatefulWidget {
   final String groupId;
@@ -56,7 +58,44 @@ class _GroupInfoState extends State<GroupInfo> {
         title: const Text("Group Info"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Exit"),
+                    content: const Text("Are you sure you exit the group? "),
+                    actions: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.cancel_rounded,
+                          color: Colors.red,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () async {
+                          DatabaseService(
+                                  uid: FirebaseAuth.instance.currentUser!.uid)
+                              .toggleGroupJoin(widget.groupId,
+                                  getName(widget.adminName), widget.groupName)
+                              .whenComplete(() {
+                            nextScreenReplace(context, const HomePage());
+                          });
+                        },
+                        icon: const Icon(
+                          Icons.done_rounded,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
             icon: Icon(Icons.exit_to_app),
           ),
         ],
